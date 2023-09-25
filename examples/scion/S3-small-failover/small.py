ae97e75815f8d36c9dd8f32b4c01373aad8ba707
MaestroOnICe <jukrebs@st.ovgu.de>
2023-09-25 20:30:30 +0200

    failover manual setup

--- examples/scion/S3-small-failover/small.py
+++ examples/scion/S3-small-failover/small.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
+from ipaddress import IPv4Network
 import time
-from seedemu.layers import ScionBase, ScionRouting, Ebgp, ScionIsd, Scion
+from seedemu.layers import ScionBase, ScionRouting, Ebgp, ScionIsd, Scion, PeerRelationship
 from seedemu.compiler import Docker, Graphviz
 from seedemu.core import Emulator
 import examples.scion.utility.utils as utils
 import examples.scion.utility.bitcoin as bitcoin
 import examples.scion.utility.experiment as experiment
 import sys
+from seedemu.layers.Scion import LinkType as ScLinkType
 
 if len(sys.argv) > 1 and sys.argv[1] == str(1):
     # Initialize the emulator and layers
@@ -32,6 +34,33 @@ if len(sys.argv) > 1 and sys.argv[1] == str(1):
     btcd = bitcoin.btcd(scion_isd)
 
 
+
+    class CrossConnectNetAssigner:
+        def __init__(self):
+            self.subnet_iter = IPv4Network("10.3.0.0/16").subnets(new_prefix=29)
+            self.xc_nets = {}
+
+        def _next_addr(self, net):
+            if net not in self.xc_nets:
+                hosts = next(self.subnet_iter).hosts()
+                next(hosts) # Skip first IP (reserved for Docker)
+                self.xc_nets[net] = hosts
+            return "{}/29".format(next(self.xc_nets[net]))
+
+    xc_nets = CrossConnectNetAssigner()
+
+    def crossNet(asn_a: int, asn_b: int, as_a_router: str="br0", as_b_router: str="br0"):
+        # cross connecting AS a <-> AS b
+        as_a = base.getAutonomousSystem(asn_a)
+        as_b = base.getAutonomousSystem(asn_b)
+        br_a = as_a.getRouter(as_a_router)
+        br_b = as_b.getRouter(as_b_router)
+        br_a.crossConnect(asn_b, as_b_router, xc_nets._next_addr(f'{asn_a}-{asn_b}'))
+        br_b.crossConnect(asn_a, as_a_router, xc_nets._next_addr(f'{asn_a}-{asn_b}'))
+
+
+
+
     as100 = maker.createTier1AS(1, 100)
     as101 = maker.createTier1AS(1, 101)
 
@@ -39,11 +68,28 @@ if len(sys.argv) > 1 and sys.argv[1] == str(1):
     as131 = maker.createTier3AS(1, 131, issuer=101)
     # Links
     ###############################################################################
-    ixp_connector.IXPConnect(20, 100)
-    ixp_connector.IXPConnect(20, 101)
+    
+    as100.getRouter("br0").joinNetwork("ix20")
+    as100.getRouter("br0").joinNetwork("ix21")
+    as101.getRouter("br0").joinNetwork("ix20")
+    as101.getRouter("br0").joinNetwork("ix21")
+    ebgp.addRsPeer(20, 100)
+    ebgp.addRsPeer(20, 101)
 
-    cross_connector.XConnect(100,130, "provider")
-    cross_connector.XConnect(101,131, "provider")
+    ebgp.addRsPeer(21, 100)
+    ebgp.addRsPeer(21, 101)
+
+    scion.addIxLink(20, (1, 100), (1, 101), ScLinkType.Core)
+    scion.addIxLink(21, (1, 100), (1, 101), ScLinkType.Core)
+
+    crossNet(100,130)
+    scion.addXcLink((1,100), (1,130), ScLinkType.Transit)
+    ebgp.addCrossConnectPeering(100, 130, PeerRelationship.Provider)
+
+
+    crossNet(101,131)
+    scion.addXcLink((1,101), (1,131), ScLinkType.Transit)
+    ebgp.addCrossConnectPeering(101, 131, PeerRelationship.Provider)
 
     btcd.creatMeasuringNode(as130, "1-131,10.131.0.80:8666")
     btcd.creatMeasuringServer(as131)
