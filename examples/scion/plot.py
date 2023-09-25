204f8b725e6cdabfadcd613232d0505be6fbe0b5
MaestroOnICe <jukrebs@st.ovgu.de>
2023-09-25 14:47:53 +0200

    add all victims plot

--- examples/scion/plot.py
+++ examples/scion/plot.py
@@ -2,23 +2,30 @@ import examples.scion.utility.plotdata as plotdata
 import os
 
 # ask for log number
-log_number = int(input("Which log directory to use: "))
+log_number = input("Which log directory to use: ")
 
 node_1_number = (input("First node: "))
 node_2_number = (input("Second node: "))
 
 cwd = os.getcwd()
-node_1_dir = f"{cwd}/data/logs_{log_number}/node_{node_1_number}/log.json"
-node_2_dir = f"{cwd}/data/logs_{log_number}/node_{node_2_number}/log.json"
+node_1_dir = f"/{cwd}/data/logs_{log_number}/node_{node_1_number}/log.json"
+node_2_dir = f"/{cwd}/data/logs_{log_number}/node_{node_2_number}/log.json"
 
 if not os.path.exists(node_1_dir):
-    node_1_dir = f"./old_logs/logs_{log_number}/node_{node_1_number}/log.json"
+    node_1_dir = f"/{cwd}/old_logs/logs_{log_number}/node_{node_1_number}/log.json"
 
 if not os.path.exists(node_2_dir):
-    node_2_dir = f"./old_logs/logs_{log_number}/node_{node_2_number}/log.json"
+    node_2_dir = f"/{cwd}/old_logs/logs_{log_number}/node_{node_2_number}/log.json"
 
 
-plotdata.plotConnectionCount(node_1_dir,node_2_dir, node_1_number, node_2_number, log_number)
-plotdata.compareChains(node_1_dir, node_2_dir, log_number)
+plotdata.plotConnectionCount(node_1_dir,node_2_dir, node_1_number, node_2_number, int(log_number))
+plotdata.compareChains(node_1_dir, node_2_dir, int(log_number))
+
+
+path_components = node_1_dir.split(os.sep)
+general_log_path = path_components[:-2]
+new_path = os.path.join(*general_log_path)
+
+plotdata.plotVictimConnectionCount(f"/{new_path}", int(log_number))
 
 #plotdata.plotCDF(f"../data/logs_{log_number}/node_101_100/log.json",log_number)
--- examples/scion/utility/plotdata.py
+++ examples/scion/utility/plotdata.py
@@ -7,7 +7,7 @@ import os
 import re
 
 
-def plotConnectionCount(file_path1: str, file_path2: str, node_1: str, node_2: str, log_number: int, ):
+def plotConnectionCount(file_path1: str, file_path2: str, node_1: str, node_2: str, log_number: int):
     # Read and parse the JSON data from the file
     with open(file_path1, 'r') as json_file:
         data_1 = json.load(json_file)["data"]
@@ -20,29 +20,89 @@ def plotConnectionCount(file_path1: str, file_path2: str, node_1: str, node_2: s
     # Create a DataFrame
     df_2 = pd.DataFrame(data_2)
 
-    # start after 10 seconds to omit ramp up
-    df_1 = df_1.iloc[1:]
-    df_2 = df_2.iloc[1:]
+
+    datapoint_offset = 1
+    # start after x seconds to omit ramp up
+    df_1 = df_1.iloc[datapoint_offset:]
+    df_2 = df_2.iloc[datapoint_offset:]
 
     # count unique connections
     df_1["uniqueConCount"] = df_1["connectedPeers"].apply(countUniques)
     df_2["uniqueConCount"] = df_2["connectedPeers"].apply(countUniques)
 
     # Plot using Seaborn
+    sns.set(rc={"figure.dpi":300, 'savefig.dpi':300})
     sns.set_theme()
-
     #fig = plt.subplot(figsize=())
     sns.lineplot(x="timeElapsed", y="uniqueConCount", data=df_1, label=node_1, linewidth=2,)
     sns.lineplot(x="timeElapsed", y="uniqueConCount", data=df_2, label=node_2, linewidth=2,)
 
-    plt.axvline(x=100, color='red', linestyle='--', label='Start')
-    plt.axvline(x=400, color='blue', linestyle='--', label='End')
+    time_Offset = datapoint_offset*10
 
+    plt.axvline(x=120-time_Offset, color='red', linestyle='--', label='Start')
+    plt.axvline(x=600-time_Offset, color='blue', linestyle='--', label='End')
+
+    # Customize the y-axis to display only integers
+    plt.yticks(range(0, max(df_1['uniqueConCount']) + 1, 1))
+    # Set the x-axis limits to start at 10 seconds
+    plt.xlim(time_Offset-10, max(df_1["timeElapsed"]))
+
+    plt.legend(fontsize=8)  # Add a legend
+    plt.title(f"Connection Count Over Time")
+    plt.xlabel("Time Elapsed (seconds)")
+    plt.ylabel("Connection Count")
+    plt.tight_layout()  # Ensure the plot fits nicely in the figure
+
+
+    fig_path = saveFigurePath(log_number)
+    plt.savefig(fig_path)
+    plt.clf()
+
+
+def plotVictimConnectionCount(log_path: str,  log_number: int):
+
+    victim_ases = ["node_130_100/log.json", "node_130_101/log.json","node_130_102/log.json", "node_130_103/log.json", "node_130_104/log.json"]
+    
+    df_dict = {}
+    for victim in victim_ases:
+        # Read and parse the JSON data from the file
+        with open(os.path.join(log_path, victim), 'r') as json_file:
+            data = json.load(json_file)["data"]
+        # Create a DataFrame
+        df = pd.DataFrame(data)
+        df["uniqueConCount"] = df["connectedPeers"].apply(countUniques)
+        df = df[["uniqueConCount", "timeElapsed"]]
+        df["victim"] = victim
+        df_dict[victim] = df 
+
+
+    # Plot using Seaborn
+    sns.set(rc={"figure.dpi":300, 'savefig.dpi':300})
+    sns.set_theme()
+
+    datapoint_offset = 1
+    # start after x seconds to omit ramp up
+
+    for key in df_dict:
+        df = df_dict[key]
+        df = df.iloc[datapoint_offset:]
+        sns.lineplot(x="timeElapsed", y="uniqueConCount", data=df, label=df["victim"], linewidth=1,legend=False)
+
+
+    time_Offset = datapoint_offset*10
+
+    plt.axvline(x=120-time_Offset, color='red', linestyle='--', label='Start')
+    plt.axvline(x=600-time_Offset, color='blue', linestyle='--', label='End')
+
+    # Customize the y-axis to display only integers
+    plt.yticks(range(0, max(df['uniqueConCount']) + 1, 1))
+    # Set the x-axis limits to start at 10 seconds
+    plt.xlim(time_Offset-10, max(df["timeElapsed"]))
 
     plt.title(f"Connection Count Over Time")
     plt.xlabel("Time Elapsed (seconds)")
     plt.ylabel("Connection Count")
-
+    #plt.tight_layout()  # Ensure the plot fits nicely in the figure
 
 
     fig_path = saveFigurePath(log_number)
@@ -64,17 +124,25 @@ def compareChains(file_path1: str, file_path2: str, log_number: int):
     # Create a DataFrame
     df_2 = pd.DataFrame(data_2)
 
-    df_1 = df_1.iloc[1:]
-    df_2 = df_2.iloc[1:]
+    # compute 
+
+    # 10 by default, beause sleep 10 for measure
+    datapoint_offset = 1
+    # start after x seconds to omit ramp up
+    df_1 = df_1.iloc[datapoint_offset:]
+    df_2 = df_2.iloc[datapoint_offset:]
 
     # Plot blockCount over time
-    sns.set_style("whitegrid")
+    sns.set(rc={"figure.dpi":300, 'savefig.dpi':300})
+    sns.set_theme()
     sns.lineplot(x="timeElapsed", y="blockCount", data=df_1, label='Blockchain 1', linewidth=2, marker='o', markersize=6, alpha=1)
     sns.lineplot(x="timeElapsed", y="blockCount", data=df_2, label='Blockchain 2', linewidth=2, marker='s', markersize=6, alpha=0.7)
-    plt.axvline(x=100, color='red', linestyle='--', label='Start')
-    plt.axvline(x=400, color='blue', linestyle='--', label='End')
+    
+    time_Offset = datapoint_offset*10
 
-    plt.legend(fontsize=12)  # Add a legend
+    plt.axvline(x=120-time_Offset, color='red', linestyle='--', label='Start')
+    plt.axvline(x=600-time_Offset, color='blue', linestyle='--', label='End')
+    plt.legend(fontsize=8)  # Add a legend
     plt.xlabel('Time Elapsed (seconds)')
     plt.ylabel('Block Count')
     plt.title('Block Count Over Time')
